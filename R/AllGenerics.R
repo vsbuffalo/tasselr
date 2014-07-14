8af738f79f0ddff5b009ba3ca6d61c38a328f94b
Vince Buffalo <vsbuffaloAAAAAA@gmail.com>
2014-07-14 16:52:37 -0700

    added generic documentation, updated namespace

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -5,24 +5,35 @@
 
 #' Load biallelic genotype information
 #'
+#' @param x a TasselHDF5 object
+#' @param verbose logical indicating whether to print status mesages during loading
 setGeneric("loadBiallelicGenotypes", function(x, verbose=TRUE) {
            standardGeneric("loadBiallelicGenotypes")
              })
 
 #' Accessor for genotype matrix
 #'
+#' @param x a TasselHDF5 object
+#' @export
 setGeneric("geno", function(x) {
            standardGeneric("geno")
              })
 
 #' Accessor for alternate alleles
+#' @param x a TasselHDF5 object
+#' @param as_char logical indicating whether to return as \code{CharacterList}
+#' or as Tassel encodes them, as an \code{IntegerList}
+#' @export
 #'
 setGeneric("alt", function(x, as_char=TRUE) {
            standardGeneric("alt")
              })
 
 #' Accessor for reference alleles
-#'
+#' @param x a TasselHDF5 object
+#' @param as_char logical indicating whether to return as character
+#' or as Tassel encodes them, as integer
+#' @export
 setGeneric("ref", function(x, as_char=TRUE) {
            standardGeneric("ref")
              })
